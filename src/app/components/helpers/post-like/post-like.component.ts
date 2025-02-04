d7f367ae9d69a58f7b9ea43b648460112e962cf8
Sergej Pejovic <s.pejovic121@gmail.com>
2025-02-04 14:23:03 +0100

    zavrsena funkcionalnost za lajkove

--- src/app/components/helpers/post-like/post-like.component.ts
+++ src/app/components/helpers/post-like/post-like.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LikeService } from '../../../services/like.service';
+import { User } from '../../../models/User';
+import { Post } from '../../../models/Post';
 
 @Component({
   selector: 'app-post-like',
   standalone: false,
-  
-  templateUrl: './post-like.component.html',
-  styleUrl: './post-like.component.scss'
-})
-export class PostLikeComponent {
 
+  templateUrl: './post-like.component.html',
+  styleUrl: './post-like.component.scss',
+})
+export class PostLikeComponent implements OnInit {
+  @Input() post: Post = new Post();
+  @Input() user: User = new User();
+  isLiked: boolean = false;
+  likesNumber: number = 0;
+
+  constructor(private likeService: LikeService) {}
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.likeService.getNumberOfLikes(this.post.id).subscribe((data: any) => {
+      if (data.length > 0) {
+        this.likesNumber = data[0].numberOfLikes;
+      } else {
+        console.log('No likes found for this post');
+      }
+    });
+  }
+
+  like(post: Post, user: User) {
+    this.likeService.createLike(post.id, user.id).subscribe((data: any) => {
+      if (data.success) {
+        this.isLiked = true;
+        console.log('Uspjesno dodat like');
+        this.loadLikes();
+      }
+    });
+  }
+
+  dislike(post: Post, user: User) {
+    this.likeService
+      .deletePostLikes(post.id, user.id)
+      .subscribe((data: any) => {
+        if (data.success) {
+          console.log('Uspjeno obrisan like');
+          this.isLiked = false;
+          this.likesNumber--;
+        }
+      });
+  }
 }
---
+++ src/app/services/like.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LikeService {
+  constructor(private http: HttpClient) {}
+
+  createLike(postId: number, userId: number) {
+    return this.http.post(
+      `http://localhost:4000/likes/${postId}/${userId}`,
+      {}
+    );
+  }
+
+  deletePostLikes(postId: number, userId: number) {
+    return this.http.delete(
+      `http://localhost:4000/likes/${postId}/dislike/${userId}`
+    );
+  }
+
+  getNumberOfLikes(postId: number) {
+    return this.http.get(`http://localhost:4000/likes/post/${postId}/likes`);
+  }
+}
